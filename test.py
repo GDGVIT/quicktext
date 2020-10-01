f9efe34dd6a61024e4720a8de15f26b25c2d47f4
ramaneswaran <ramannewdelhi44@gmail.com>
2020-10-02 02:39:17 +0530

    Test BiLSTM initialization

--- test.py
+++ test.py
@@ -2,7 +2,11 @@ import pandas as pd
 import numpy as np
 
 from torch.utils.data import DataLoader
+
 from qtc.data import TextClassifierData
+from qtc.nets import BiLSTM
+
+from pytorch_lightning import trainer
 
 df_path = "../data/aclImdb/train/data.csv"
 df = pd.read_csv(df_path)
@@ -10,10 +14,14 @@ df = pd.read_csv(df_path)
 texts = df["text"].tolist()
 labels = df["label"].tolist()
 
-data = TextClassifierData(texts=texts, labels=labels)
-loader = DataLoader(data, batch_size=4, collate_fn=data.get_batch)
+# data = TextClassifierData(texts=texts, labels=labels)
+# loader = DataLoader(data, batch_size=4, collate_fn=data.get_batch)
 
-batch = next(iter(loader))
+EMBEDDING_DIM = 300
+HIDDEN_DIM = 128
+OUTPUT_DIM = 2
+N_LAYERS = 2
+BIDIRECTIONAL = True
+DROPOUT = 0.5
 
-print(batch["texts"].shape)
-print(batch["seq_lens"])
+model = BiLSTM(EMBEDDING_DIM, OUTPUT_DIM, HIDDEN_DIM, N_LAYERS, BIDIRECTIONAL, DROPOUT,)
