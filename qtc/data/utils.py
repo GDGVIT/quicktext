aa62dd1a5753bdcf6a16dafebdd0d81a92423756
ramaneswaran <ramannewdelhi44@gmail.com>
2020-10-01 22:44:14 +0530

    Add Vocab class

---
+++ qtc/data/utils.py
@@ -0,0 +1,47 @@
+from qtc import *
+
+
+class Vocab:
+    """
+    This class helps in building vocabulary 
+    """
+
+    def __init__(self, featurizer, pad_token="@pad@"):
+        """
+        Constructor function for Vocab class
+        Args:
+            featurizer (SpacyFeaturizer): Featurizer class, contains untility methods 
+                                            to build vocabulary
+            pad_token (string): Token used for padding
+        Returns:
+            None
+        """
+
+        self.featurizer = featurizer
+
+    def build(self, texts):
+        """
+        This function builds vocabulary using the input list of text
+        Args:
+            texts (List): List of texts
+        Return:
+            dict: A dictionary of words 
+        """
+
+        print("[INFO] Building the vocabulary")
+
+        # Tokenize the text
+        self.texts = [self.featurizer.tokenize(text) for text in tqdm(texts)]
+
+        # Build dictionary of word -> index
+        self.stoi = {}
+        self.stoi["@pad@"] = 0
+        index = 1
+        for text in self.texts:
+            for token in text:
+                self.stoi[token] = index
+                index += 1
+
+        self.itos = {idx: word for (word, idx) in self.stoi.items()}
+
+        return self.stoi, self.itos
