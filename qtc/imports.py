fe53a3c63051c7e781e7a479790e55fccbed785e
ramaneswaran <ramannewdelhi44@gmail.com>
2020-10-02 02:18:45 +0530

    Add import for torch.nn

--- qtc/imports.py
+++ qtc/imports.py
@@ -17,7 +17,9 @@ import en_core_web_md
 
 # Torch imports
 import torch
+import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
+
 
 # Pytorch lightning
 import pytorch_lightning as pl
