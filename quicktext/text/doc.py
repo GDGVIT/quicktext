aa1cc964915d83c5cd484b9b27bcf24efa511165
ramaneswaran <ramannewdelhi44@gmail.com>
2020-10-03 21:03:38 +0530

    Format Doc class

--- quicktext/text/doc.py
+++ quicktext/text/doc.py
@@ -1,11 +1,12 @@
 from quicktext.imports import *
 
+
 class Doc:
     """
     This class provides a container to store the tokens and 
     ids for text in the dataset
     """
-    
+
     def __init__(self, text):
         """
         Contructor function for Doc class
@@ -23,19 +24,17 @@ class Doc:
     @property
     def ids(self):
         return self._ids
-    
+
     @ids.setter
     def ids(self, new_ids):
         if isinstance(new_ids, list):
             self._ids = new_ids
-        
+
     @property
     def tokens(self):
-        return self._tokens 
-    
+        return self._tokens
+
     @tokens.setter
     def tokens(self, new_tokens):
         if isinstance(new_tokens, list):
             self._tokens = new_tokens
-
-    
