93ecaf5fc08092b6cfd5415564020333b6cca868
ramaneswaran <ramannewdelhi44@gmail.com>
2020-10-04 15:17:06 +0530

    Clean up data.utils module

--- quicktext/data/utils.py
+++ quicktext/data/utils.py
@@ -1,92 +1,9 @@
 from quicktext.imports import *
 
-__all__ = ["Vocab", "_prepare_label"]
+__all__ = ["prepare_label"]
 
 
-class Vocab:
-    """
-    This class helps in building vocabulary 
-    """
-
-    def __init__(self, featurizer, pad_token="@pad@"):
-        """
-        Constructor function for Vocab class
-        Args:
-            featurizer (SpacyFeaturizer): Featurizer class, contains untility methods 
-                                            to build vocabulary
-            pad_token (string): Token used for padding
-        Returns:
-            None
-        """
-
-        self.featurizer = featurizer
-
-    def build(self, texts):
-        """
-        This function builds vocabulary using the input list of text
-        Args:
-            texts (List): List of texts
-        Return:
-            dict: A dictionary of words 
-        """
-
-        print("[INFO] Building the vocabulary")
-
-        # Tokenize the text
-        self._texts = [self.featurizer.tokenize(text) for text in tqdm(texts)]
-
-        # Build dictionary of word -> index
-        self.stoi = {}
-        self.stoi["@pad@"] = 0
-        index = 1
-        for _text in self._texts:
-            for token in _text:
-                self.stoi[token] = index
-                index += 1
-
-        self.itos = {idx: word for (word, idx) in self.stoi.items()}
-
-    def get_tokenized_texts(self):
-        """
-        This function returns the tokenized text
-        Args:
-            None
-        Returns:
-            list: List of tokenized text
-        """
-
-        self._idx_texts = []
-        for text in self._texts:
-            _text = [self.stoi[token] for token in text if token in self.stoi]
-
-            self._idx_texts.append(_text)
-
-        return self._idx_texts
-
-    def get_stoi(self):
-        """
-        This function returns the stoi dictionary
-        Args:
-            None
-        Returns:
-            dict: The stoi dictionary
-        """
-
-        return self.stoi
-
-    def get_itos(self):
-        """
-        This function returns the itos dictionary
-        Args:
-            None
-        Returns:
-            dict: The itos dictionary
-        """
-
-        return self.itos
-
-
-def _prepare_labels(labels):
+def prepare_labels(labels):
     """
     This function converts labels to a format that can be 
     used by PyTorch models
