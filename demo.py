1105aa2bae38aada650b676a6c905ee2e67993ff
ramaneswaran <ramannewdelhi44@gmail.com>
2020-11-06 20:25:05 +0530

    Rename test to demo: Code works

---
+++ demo.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import spacy
+from sklearn.model_selection import train_test_split
+
+from quicktext.classifier.text_classifier import TextClassifier
+from quicktext.engine.trainer import Trainer
+
+path = r"../data/aclImdb/train/data.csv"
+
+print("[INFO] Loading spacy model")
+nlp = spacy.load("en_core_web_md")
+
+print("[INFO] Loading classifier data")
+classifier = TextClassifier(nlp.vocab, arch="cnn", classes=[0, 1], hparams={"param": 0})
+
+
+print("[INFO] Preparing training data")
+df = pd.read_csv(path)
+data = [(row[0], row[1]) for index, row in df.iterrows()]
+
+train_data, test_data = train_test_split(data, test_size=0.2, random_state=1)
+
+train_data, val_data = train_test_split(
+    train_data, test_size=0.25, random_state=1
+)  # 0.25 x 0.8 = 0.2
+
+
+print("[INFO] Training data")
+trainer = Trainer(classifier, train_data, val_data, test_data, batch_size=2)
+trainer.fit(epochs=1, gpus=0)
