50ff297a3a8fa8418cf3f0e919e3b9b01d7638cc
ramaneswaran <ramannewdelhi44@gmail.com>
2020-11-21 09:48:29 +0530

    Add tests for dataset

---
+++ tests/test_dataset.py
@@ -0,0 +1,16 @@
+import unittest
+from quicktext.datasets import get_imdb,  get_20newsgroups
+
+class TextClassifierTester(unittest.TestCase):
+    def test_imdb_download(self):
+        imdb = get_imdb()
+        self.assertTrue(isinstance(imdb.train.data, list))
+        # self.assertTrue(isinstance(imdb.train.data[0], str))
+    
+    def test_newsgroups_download(self):
+        newsgroups = get_20newsgroups()
+        self.assertTrue(isinstance(newsgroups.train.data, list))
+        # self.assertTrue(isinstance(newsgroups.train.data[0], str))
+    
+if __name__ == "__main__":
+    unittest.main()
