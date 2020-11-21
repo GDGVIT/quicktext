802a4aa1eb0660af88ae23084c587e76e796b8a4
ramaneswaran <ramannewdelhi44@gmail.com>
2020-11-21 10:24:33 +0530

    Add more tests for dataset

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -5,12 +5,12 @@ class TextClassifierTester(unittest.TestCase):
     def test_imdb_download(self):
         imdb = get_imdb()
         self.assertTrue(isinstance(imdb.train.data, list))
-        # self.assertTrue(isinstance(imdb.train.data[0], str))
+        self.assertTrue(isinstance(imdb.train.data[0], str))
     
     def test_newsgroups_download(self):
         newsgroups = get_20newsgroups()
         self.assertTrue(isinstance(newsgroups.train.data, list))
-        # self.assertTrue(isinstance(newsgroups.train.data[0], str))
+        self.assertTrue(isinstance(newsgroups.train.data[0], str))
     
 if __name__ == "__main__":
     unittest.main()
